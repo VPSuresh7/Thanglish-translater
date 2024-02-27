851c1013bda691d6189fc8324012d79e3356991e
VPSuresh7 <120002062+VPSuresh7@users.noreply.github.com>
2024-02-27 11:20:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 
 from flask import Flask, request, jsonify
 import Py_Thanglish as py
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 
 @app.route("/")  
@@ -9,6 +12,7 @@ def home():
     return "Hello you are in Thanglish translater!!" 
 
 @app.post('/translate')
+@cross_origin(supports_credentials=True)
 def translate():
     data = request.get_json()
     input = data["text"]
